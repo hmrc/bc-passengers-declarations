9431d4b60b8a9c8d7895e6c273e8757b872aadf7
Nick Wilson <thenickwilson@gmail.com>
2018-11-05 08:47:53 +0000

    Introduce BaseDeclarationWorker

--- app/workers/DeclarationSubmissionWorker.scala
+++ app/workers/DeclarationSubmissionWorker.scala
@@ -15,52 +15,44 @@ import scala.util.control.NonFatal
 
 class DeclarationSubmissionWorker @Inject() (
                                               declarationsRepository: DeclarationsRepository,
-                                              lockRepository: LockRepository,
+                                              override protected val lockRepository: LockRepository,
                                               hodConnector: HODConnector,
                                               config: Configuration
-                                            )(implicit mat: Materializer, ec: ExecutionContext) {
+                                            )(implicit mat: Materializer, ec: ExecutionContext)
+  extends BaseDeclarationWorker {
 
-  private val initialDelay = config.get[FiniteDuration]("workers.declaration-submission-worker.initial-delay")
-  private val interval = config.get[FiniteDuration]("workers.declaration-submission-worker.interval")
-  private val parallelism = config.get[Int]("workers.declaration-submission-worker.parallelism")
-  private val elements = config.get[Int]("workers.declaration-submission-worker.throttle.elements")
-  private val per = config.get[FiniteDuration]("workers.declaration-submission-worker.throttle.per")
+    private val initialDelay = config.get[FiniteDuration]("workers.declaration-submission-worker.initial-delay")
+    private val interval = config.get[FiniteDuration]("workers.declaration-submission-worker.interval")
+    private val parallelism = config.get[Int]("workers.declaration-submission-worker.parallelism")
+    private val elements = config.get[Int]("workers.declaration-submission-worker.throttle.elements")
+    private val per = config.get[FiniteDuration]("workers.declaration-submission-worker.throttle.per")
 
-  private val supervisionStrategy: Supervision.Decider = {
-    case NonFatal(_) => Supervision.resume
-    case _           => Supervision.stop
-  }
-
-  val tap: SinkQueueWithCancel[(Declaration, SubmissionResponse)] =
-    Source.tick(initialDelay, interval, declarationsRepository.paidDeclarations)
-      .flatMapConcat(identity)
-      .throttle(elements, per)
-      .mapAsync(parallelism)(getLock)
-      .mapConcat(lockSuccessful)
-      .mapAsync(parallelism) {
-        declaration =>
-          for {
-            result <- hodConnector.submit(declaration)
-            _      <- result match {
-              case SubmissionResponse.Submitted =>
-                declarationsRepository.remove(declaration.chargeReference)
-              case SubmissionResponse.Error =>
-                Future.successful(())
-              case SubmissionResponse.Failed =>
-                declarationsRepository.setState(declaration.chargeReference, State.Failed)
-            }
-          } yield (declaration, result)
-      }.wireTapMat(Sink.queue())(Keep.right)
-        .toMat(Sink.ignore)(Keep.left)
-        .withAttributes(ActorAttributes.supervisionStrategy(supervisionStrategy))
-        .run()
-
-  private def getLock(declaration: Declaration): Future[(Boolean, Declaration)] =
-    lockRepository.lock(declaration.chargeReference.value).map(_ -> declaration)
-
-  private def lockSuccessful(data: (Boolean, Declaration)): List[Declaration] =
-    data match {
-      case (hasLock, declaration) =>
-        if (hasLock) List(declaration) else Nil
+    private val supervisionStrategy: Supervision.Decider = {
+      case NonFatal(_) => Supervision.resume
+      case _           => Supervision.stop
     }
+
+    val tap: SinkQueueWithCancel[(Declaration, SubmissionResponse)] =
+      Source.tick(initialDelay, interval, declarationsRepository.paidDeclarations)
+        .flatMapConcat(identity)
+        .throttle(elements, per)
+        .mapAsync(parallelism)(getLock)
+        .mapConcat(lockSuccessful)
+        .mapAsync(parallelism) {
+          declaration =>
+            for {
+              result <- hodConnector.submit(declaration)
+              _      <- result match {
+                case SubmissionResponse.Submitted =>
+                  declarationsRepository.remove(declaration.chargeReference)
+                case SubmissionResponse.Error =>
+                  Future.successful(())
+                case SubmissionResponse.Failed =>
+                  declarationsRepository.setState(declaration.chargeReference, State.Failed)
+              }
+            } yield (declaration, result)
+        }.wireTapMat(Sink.queue())(Keep.right)
+          .toMat(Sink.ignore)(Keep.left)
+          .withAttributes(ActorAttributes.supervisionStrategy(supervisionStrategy))
+          .run()
 }
--- app/workers/FailedSubmissionWorker.scala
+++ app/workers/FailedSubmissionWorker.scala
@@ -1,47 +1,39 @@
 package workers
 
-import akka.stream.{ActorAttributes, Materializer, Supervision}
 import akka.stream.scaladsl.{Keep, Sink, SinkQueueWithCancel}
+import akka.stream.{ActorAttributes, Materializer, Supervision}
 import javax.inject.Inject
 import models.declarations.{Declaration, State}
 import play.api.Configuration
 import repositories.{DeclarationsRepository, LockRepository}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.util.control.NonFatal
 
 class FailedSubmissionWorker @Inject()(
                                         declarationsRepository: DeclarationsRepository,
-                                        lockRepository: LockRepository,
+                                        override protected val lockRepository: LockRepository,
                                         config: Configuration
-                                      )(implicit ec: ExecutionContext, mat: Materializer) {
+                                      )(implicit ec: ExecutionContext, mat: Materializer)
+  extends BaseDeclarationWorker {
 
-  private val parallelism = config.get[Int]("workers.failed-submission-worker.parallelism")
+    private val parallelism = config.get[Int]("workers.failed-submission-worker.parallelism")
 
-  private val decider: Supervision.Decider = {
-    case NonFatal(_) => Supervision.resume
-    case _           => Supervision.stop
-  }
-
-  val tap: SinkQueueWithCancel[Declaration] =
-    declarationsRepository.failedDeclarations
-      .mapAsync(parallelism)(getLock)
-      .filter(lockSuccessful)
-      .mapAsync(parallelism) {
-        case (_, declaration) =>
-
-          declarationsRepository.setState(declaration.chargeReference, State.Paid)
-      }.wireTapMat(Sink.queue())(Keep.right)
-        .toMat(Sink.ignore)(Keep.left)
-        .withAttributes(ActorAttributes.supervisionStrategy(decider))
-        .run()
-
-  private def getLock(declaration: Declaration): Future[(Boolean, Declaration)] =
-    lockRepository.lock(declaration.chargeReference.value).map(_ -> declaration)
-
-  private def lockSuccessful(data: (Boolean, Declaration)): Boolean =
-    data match {
-      case (hasLock, _) =>
-        hasLock
+    private val decider: Supervision.Decider = {
+      case NonFatal(_) => Supervision.resume
+      case _           => Supervision.stop
     }
+
+    val tap: SinkQueueWithCancel[Declaration] =
+      declarationsRepository.failedDeclarations
+        .mapAsync(parallelism)(getLock)
+        .mapConcat(lockSuccessful)
+        .mapAsync(parallelism) {
+          declaration =>
+
+            declarationsRepository.setState(declaration.chargeReference, State.Paid)
+        }.wireTapMat(Sink.queue())(Keep.right)
+          .toMat(Sink.ignore)(Keep.left)
+          .withAttributes(ActorAttributes.supervisionStrategy(decider))
+          .run()
 }
