4dbd2c16aca4c89e9680c568c5a818c86ae95868
Michael Wolfendale <michael.wolfendale@gmail.com>
2018-11-06 09:38:08 +0000

    Add resource service

---
+++ app/services/ResourceService.scala
@@ -0,0 +1,26 @@
+package services
+
+import java.io.IOException
+
+import com.google.inject.{Inject, Singleton}
+import play.api.Environment
+import play.api.libs.json.{JsValue, Json}
+
+import scala.io.Source
+
+@Singleton
+class ResourceService @Inject() (
+                                  environment: Environment
+                                ) {
+
+  def getFile(resource: String): String = {
+
+    val stream = environment.resourceAsStream(resource)
+      .getOrElse(throw new IOException(s"resource not found: $resource"))
+
+    Source.fromInputStream(stream).mkString
+  }
+
+  def getJson(resource: String): JsValue =
+    Json.parse(getFile(resource))
+}
