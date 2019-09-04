5d3814ad3159176ea21662815a4e62515a6baaf1
Nic Fellows <nic@carboninternet.net>
2019-09-04 13:57:50 +0100

    PAS-698 Upgrade project dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,20 +7,20 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"                %% "bootstrap-play-26"        % "0.39.0",
-    "com.github.java-json-tools" % "json-schema-validator"     % "2.2.8",
-    "org.reactivemongo"          %% "play2-reactivemongo"      % "0.16.0-play26",
-    "org.reactivemongo"          %% "reactivemongo-akkastream" % "0.16.1"
+    "uk.gov.hmrc"                %% "bootstrap-play-26"        % "0.46.0",
+    "com.github.java-json-tools" % "json-schema-validator"     % "2.2.10",
+    "org.reactivemongo"          %% "play2-reactivemongo"      % "0.18.5-play26",
+    "org.reactivemongo"          %% "reactivemongo-akkastream" % "0.18.5"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"            %% "scalatest"                % "3.0.4",
+    "org.scalatest"            %% "scalatest"                % "3.0.8",
     "com.typesafe.play"        %% "play-test"                % current,
     "org.pegdown"              %  "pegdown"                  % "1.6.0",
-    "org.mockito"              %  "mockito-all"              % "1.10.19",
-    "com.github.tomakehurst"   %  "wiremock-standalone"      % "2.17.0",
-    "com.github.netcrusherorg" % "netcrusher-core"           % "0.10",
-    "org.scalatestplus.play"   %% "scalatestplus-play"       % "2.0.0",
-    "org.scalacheck"           %% "scalacheck"               % "1.13.4"
+    "org.mockito"              %  "mockito-all"              % "2.0.2-beta",
+    "com.github.tomakehurst"   %  "wiremock-standalone"      % "2.24.1",
+    "com.github.netcrusherorg" %  "netcrusher-core"          % "0.10",
+    "org.scalatestplus.play"   %% "scalatestplus-play"       % "3.1.2",
+    "org.scalacheck"           %% "scalacheck"               % "1.14.0"
   ).map(_ % "test,it")
 }
