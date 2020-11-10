048abdbebc7706391c47c9ac8da1e7cfb9fae3f3
anbu.roach <60651188+anburoach@users.noreply.github.com>
2020-11-10 14:33:42 +0000

    [PAS-810]:Send GB-NI Data Items to ETMP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"                %% "bootstrap-play-26"        % "1.14.0",
+    "uk.gov.hmrc"                %% "bootstrap-play-26"        % "2.0.0",
     "com.github.java-json-tools" % "json-schema-validator"     % "2.2.14",
     "org.reactivemongo"          %% "play2-reactivemongo"      % "0.18.8-play26",
     "org.reactivemongo"          %% "reactivemongo-akkastream" % "0.18.8",
@@ -15,11 +15,11 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"            %% "scalatest"                % "3.0.8",
+    "org.scalatest"            %% "scalatest"                % "3.0.9",
     "com.typesafe.play"        %% "play-test"                % current,
     "org.pegdown"              %  "pegdown"                  % "1.6.0",
     "org.mockito"              %  "mockito-all"              % "2.0.2-beta",
-    "com.github.tomakehurst"   %  "wiremock-standalone"      % "2.27.1",
+    "com.github.tomakehurst"   %  "wiremock-standalone"      % "2.27.2",
     "com.github.netcrusherorg" %  "netcrusher-core"          % "0.10",
     "org.scalatestplus.play"   %% "scalatestplus-play"       % "3.1.3",
     "org.scalacheck"           %% "scalacheck"               % "1.14.3"
